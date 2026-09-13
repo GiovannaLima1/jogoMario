a38551a1794990835e272f5dae060da45df50de5
gabriellaguedesaa-source <gabriellaguedesaa@gmail.com>
2026-09-13 11:35:23 -0300

    feat: implementa movimentacao e colisao do Mario

---
+++ frontend/js/script.js
@@ -0,0 +1,31 @@
+const mario = document.querySelector('.mario');
+const pipe = document.querySelector('.pipe');
+
+const jump = () => {
+    mario.classList.add('jump');
+
+    setTimeout(() => {
+        mario.classList.remove('jump');
+    }, 500);
+};
+
+const loop = setInterval(() => {
+    const pipePosition = pipe.offsetLeft;
+    const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', '');
+
+    if (pipePosition <= 150 && pipePosition > 0 && marioPosition < 80) {
+        pipe.style.animation = 'none';
+        pipe.style.right = `${pipePosition}px`;
+
+        mario.style.animation = 'none';
+        mario.style.bottom = `${marioPosition}px`;
+
+        mario.src = './images/game-over.png';
+        mario.style.width = '75px';
+
+        clearInterval(loop);
+    }
+}, 10);
+
+document.addEventListener('keydown', jump);
+document.addEventListener('touchstart', jump);
